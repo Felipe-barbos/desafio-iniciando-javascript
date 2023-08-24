2a5b26305d1c4601ebf64a941b771a4347eb3425
Felipe-barbos <felipe.csultimate@gmail.com>
2023-08-24 16:16:29 -0300

    feat:Desafio concluído

---
+++ scripts.js
@@ -0,0 +1,118 @@
+
+
+//alert("Hello World!");
+
+
+/*
+
+let num1 = Number(prompt("Digite o primeiro valor: "));
+let num2 = Number(prompt("Digite o segundo valor :"));
+
+alert(`Resultado da soma dos dois valores: ${num1 + num2}`);
+
+*/
+
+/*
+let value = 15;
+
+if (typeof (value) === "number") {
+  alert("É um número");
+}
+else {
+  alert("Não é um número");
+}
+
+*/
+
+/*
+
+let value = "Felipe"
+
+if (typeof (value) === "string") {
+  alert("É uma string")
+}
+else {
+  alert("Não é uma estring")
+}
+
+*/
+
+/*
+
+let value = true;
+
+if (typeof (value) === "boolean") {
+  alert("É um booleano")
+}
+else {
+  alert("Não é um booleano")
+}
+
+*/
+
+
+/*
+
+let num1 = Number(prompt("Digite o primeiro valor: "));
+let num2 = Number(prompt("Digite o segundo valor :"));
+
+alert(`Resultado da soma dos dois valores: ${num1 - num2}`);
+
+*/
+
+
+
+/*
+
+let num1 = Number(prompt("Digite o primeiro valor: "));
+let num2 = Number(prompt("Digite o segundo valor :"));
+
+alert(`Resultado da soma dos dois valores: ${num1 * num2}`);
+
+*/
+
+
+
+/*
+
+let num1 = Number(prompt("Digite o primeiro valor: "));
+let num2 = Number(prompt("Digite o segundo valor :"));
+
+alert(`Resultado da soma dos dois valores: ${num1 / num2}`);
+
+*/
+
+
+
+
+/*
+
+
+let value = Number(prompt("Digite um número:"));
+
+
+
+
+if (value % 2 === 0) {
+  alert("O número que tu digitou é um número PAR!");
+} else {
+  alert("O número que tu digitou não é um número PAR!")
+}
+
+*/
+
+/*
+
+let value = Number(prompt("Digite um número:"));
+
+
+
+
+if (value % 2 !== 0) {
+  alert("O número que tu digitou é um número IMPAR!");
+} else {
+  alert("O número que tu digitou não é um número IMPAR!")
+}
+
+
+*/
